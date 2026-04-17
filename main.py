aa95dd8bec8eaefacd5ab045a19aba1f2abe5a66
lv1hn <26luhan@naver.com>
2026-04-17 16:56:22 +0900

    Update main.py
    ODSay가 안 열려요

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ def get_route(sx, sy, ex, ey):
     url = f"https://api.odsay.com/v1/api/searchPubTransPathT?SX={sx}&SY={sy}&EX={ex}&EY={ey}&apiKey={ODSAY_API_KEY}"
     headers = {
         "Referer": "https://naona.onrender.com"
+        "User-Agent": "Mozilla/5.0"
     }
     try:
         respraw = requests.get(url)
